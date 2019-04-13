f0729031989bd5f0674d2f7f79d14346a48dd3dd
Zemian Deng <zemian.deng@oracle.com>
2019-04-13 10:30:01 -0400

    Update greeter with OO

--- typescript-starter/greeter.ts
+++ typescript-starter/greeter.ts
@@ -1,8 +1,19 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
 
-let user = "Jane User";
-//let user = [1, 2, 3];
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
