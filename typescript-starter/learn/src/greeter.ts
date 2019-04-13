6cf3f5ed2380496cd25feb4a831a28a28e515e32
Zemian Deng <zemian.deng@oracle.com>
2019-04-13 11:11:24 -0400

    Add learn

---
+++ typescript-starter/learn/src/greeter.ts
@@ -0,0 +1,21 @@
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
