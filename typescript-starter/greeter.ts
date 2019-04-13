10c82e2dbf33dc4f1250b4815ed19ec3864d6b5d
Zemian Deng <zemian.deng@oracle.com>
2019-04-13 10:23:34 -0400

    Update greeter and readme

--- typescript-starter/greeter.ts
+++ typescript-starter/greeter.ts
@@ -1,7 +1,8 @@
-function greeter(person) {
+function greeter(person: string) {
     return "Hello, " + person;
 }
 
 let user = "Jane User";
+//let user = [1, 2, 3];
 
 document.body.innerHTML = greeter(user);
